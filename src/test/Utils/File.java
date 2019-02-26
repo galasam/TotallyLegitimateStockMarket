6681563f589b0a3ab90c54a81c065d1a9853134e
srussell <samuel.russell@galatea-associates.com>
2019-02-26 11:51:19 +0000

    gitignore

---
+++ src/test/Utils/File.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Utils {
+
+    static String readTestFile(String filename) {
+       throw new UnsupportedOperationException("Note implemented");
+    }
+
+    static String writeTestFile(String filename, String contents) {
+        throw new UnsupportedOperationException("Note implemented");
+    }
+
+}
